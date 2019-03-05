a79d92c9d644aeab7aae849d662afe0a990b806b
Krzysztof Kruk <krzysztof.pawel.kruk@gmail.com>
2019-03-05 22:29:33 +0100

    Add tests

---
+++ l1/tests/test_core_model.cpp
@@ -0,0 +1,8 @@
+#define BOOST_TEST_MODULE test_core_model
+#include <boost/test/included/unit_test.hpp>
+#include "serialport.h"
+
+BOOST_AUTO_TEST_CASE(first_uber_test)
+{
+    BOOST_TEST(true);
+}
