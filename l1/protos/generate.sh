65b66a0f4f75d2391d13d9087ddf627f28f37e76
Krzysztof Kruk <krzysztof.pawel.kruk@gmail.com>
2019-03-24 17:11:03 +0100

    Add clean cmd to generate command

--- l1/protos/generate.sh
+++ l1/protos/generate.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-protoc --grpc_out=. --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` *.proto
-protoc --cpp_out=. *.proto
-python -m grpc_tools.protoc -I./ --python_out=. --grpc_python_out=. *.proto
+if [[ $1 == 'clean' ]]; then
+    to_delete='*.grpc.pb.cc *.grpc.pb.h *_pb2_grpc.py *_pb2.py *.pb.cc *.pb.h'
+    for ending in $to_delete; do
+        rm $ending
+    done
+else
+    protoc --grpc_out=. --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` *.proto
+    protoc --cpp_out=. *.proto
+    python -m grpc_tools.protoc -I./ --python_out=. --grpc_python_out=. *.proto
+fi
